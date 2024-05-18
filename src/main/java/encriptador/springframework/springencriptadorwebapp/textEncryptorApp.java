555da0b9647a6348e5e8cbb1b0ed86ad00e7a5b9
Edgar Martínez <86424705+DanielMMITM@users.noreply.github.com>
2024-05-17 18:25:44 -0600

    Update - update project file structure
    Update files to be in the root directory

---
+++ src/main/java/encriptador/springframework/springencriptadorwebapp/textEncryptorApp.java
@@ -0,0 +1,13 @@
+package encriptador.springframework.springencriptadorwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class textEncryptorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(textEncryptorApp.class, args);
+	}
+
+}
---
+++ src/test/java/encriptador/springframework/springencriptadorwebapp/SpringencriptadorwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package encriptador.springframework.springencriptadorwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringencriptadorwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
